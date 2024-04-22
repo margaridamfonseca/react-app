6f17dd853dafb252a3b2adeb97e27350de6ead8b
margaridamfonseca <164942212+margaridamfonseca@users.noreply.github.com>
2024-04-22 21:48:52 +0100

    updated day and time based on api

--- src/Weather-data.js
+++ src/Weather-data.js
@@ -1,13 +1,11 @@
 import React, {useState} from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 
 export default function Weatherdata () {
     
     let [weatherData, setWeatherData] = useState({ready: false});    
-    let [city, setCity] = useState("Lisbon");
-    let [date, setDate] = useState("MON");
-    let [time, setTime] = useState("12:00");
-    
+    let [city, setCity] = useState("Lisbon");  
     
 
     function updateWeather(response){
@@ -16,13 +14,9 @@ export default function Weatherdata () {
             humidity: response.data.temperature.humidity,
             wind: response.data.wind.speed,
             icon: <img src={response.data.condition.icon_url} alt={response.data.condition.description}/>,
-            ready: true
-           
-        });
-        
-        setDate("lala");
-        setTime("lala");        
-        setReady(true);     
+            ready: true,
+            date: new Date(response.data.time),          
+        });                    
     }    
 
     function handleSubmit(event){
@@ -32,7 +26,7 @@ export default function Weatherdata () {
     function updateCity(event){
         setCity(event.target.value);
     }
-    if (ready){    
+    if (weatherData.ready){    
     return (
         <div>
         <div className="Form"> 
@@ -47,13 +41,12 @@ export default function Weatherdata () {
                 {city}
             </h1>
             <p className="weather-app-details">
-                <span>{date}</span>, <span>{time}</span>
-                <br/>
+                <FormattedDate date={weatherData.date}/>
                 Humidity: <strong>{weatherData.humidity}%</strong>, Wind: <strong>{weatherData.wind}km/h</strong>
             </p>
         </div>
         <div className= "weather-app-temperature-container">
-            <div>{weatherData.icon}</div>
+            <div className="weather-app-temperature-icon">{weatherData.icon}</div>
             <div className= "weather-app-temperature-value">{weatherData.temperature}</div>
             <div className= "weather-app-temperature-unit">ºC|F</div>
         </div>
