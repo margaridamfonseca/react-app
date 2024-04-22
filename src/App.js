7dce49bc355bb9911f73fad807f5d6b32a53ffb8
margaridamfonseca <164942212+margaridamfonseca@users.noreply.github.com>
2024-04-23 00:09:19 +0100

    cleaned unnecessary files for fake data forecast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Weatherdata from "./Weather-data";
-import Forecast from "./Forecast";
+
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
     <div className="weather-app"> 
       <main>
           <Weatherdata city="Lisbon"/>
-          <Forecast />                
+                     
       </main>     
       <footer>
           This project was coded by <a href= "https://github.com/margaridamfonseca" target="_blank" rel="noreferrer"> Margarida Fonseca </a>, is open-sourced on <a href="https://github.com/margaridamfonseca/my-weather-app-redo" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://my-weather-app-redo.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
