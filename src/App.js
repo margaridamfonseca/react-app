407087c6c3c1a5a53cba1ca85f98258b1e405fa7
margaridamfonseca <164942212+margaridamfonseca@users.noreply.github.com>
2024-04-22 14:44:05 +0100

    added bootstrap through npm

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Forecast from "./Forecast";
 function App() {
   return (
     <div className="App">
+    <div className="container">
     <div className="weather-app"> 
       <header> 
          <Form /> 
@@ -17,6 +18,7 @@ function App() {
       <footer>
           This project was coded by <a href= "https://github.com/margaridamfonseca" target="_blank" rel="noreferrer"> Margarida Fonseca </a>, is open-sourced on <a href="https://github.com/margaridamfonseca/my-weather-app-redo" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://my-weather-app-redo.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
       </footer>
+  </div>
   </div>
   </div>
   );
