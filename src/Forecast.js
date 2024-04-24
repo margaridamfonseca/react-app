5da64a5b233533b3cba886e1090e1030c1b753c7
margaridamfonseca <164942212+margaridamfonseca@users.noreply.github.com>
2024-04-24 14:32:27 +0100

    fixed bug in forecast -> update whenever city changes

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -7,7 +7,8 @@ export default function Forecast(props){
 
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState("");
-    
+
+    useEffect(() => {setLoaded(false)}, [props.city]);   
 
     function updateForecast(response){
         setForecast(response.data.daily);
